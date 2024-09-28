ac00254e23cc4d762cded5d572e2e79ef8dc8860
HeyItsJono <freymotifs@gmail.com>
2024-09-29 07:30:32 +1000

    Fix cache feature.

--- plugin/calcs.py
+++ plugin/calcs.py
@@ -1,7 +1,7 @@
 import datetime
+import json
 import os
 import re
-from json import loads
 
 import requests
 from bs4 import BeautifulSoup
@@ -15,7 +15,7 @@ def fetch():
     # A json object containing a dict of all calcs, as well as some other relevant info, is stored in a script tag with the id "__NEXT_DATA__" right at the bottom of the page.
     # soup.find() returns a bs4 tag object, .contents[0] returns the actual text within that tag, but this is returned as a Script object and must be converted to a string.
     # Once it is a string, it can be converted to a nested dictionary with json.loads() making it easy to work with.
-    mdcalcs = loads(str(soup.find("script", id="__NEXT_DATA__").contents[0]))
+    mdcalcs = json.loads(str(soup.find("script", id="__NEXT_DATA__").contents[0]))
 
     # Big variables, no longer needed.
     del request, soup
@@ -42,11 +42,11 @@ def fetch():
 
 
 def cache(age=7):
-    # Check for the existence of a cache file. If it exists, check if it was generated less than X days ago (default = 1wk). If it was, return the cached calcs.
+    # Check for the existence of a cache file. If it exists, check if it was generated less than 'age' days ago (default = 1wk). If it was, return the cached calcs.
     cache_file = "cache.json"
     if os.path.exists(cache_file):
         with open(cache_file, "r") as f:
-            cache = loads(f.read())
+            cache = json.load(f)
             if datetime.datetime.now() - datetime.datetime.fromisoformat(
                 cache["timestamp"]
             ) < datetime.timedelta(days=age):
@@ -55,15 +55,11 @@ def cache(age=7):
     # If the cache file doesn't exist, or is older than a week, fetch the calcs, write them to the cache file, and return them.
     calcs = fetch()
     with open(cache_file, "w") as f:
-        f.write(
-            '{"calcs": '
-            + str(calcs)
-            + ', "timestamp": "'
-            + str(datetime.datetime.now().isoformat())
-            + '"}'
+        json.dump(
+            {"calcs": calcs, "timestamp": str(datetime.datetime.now().isoformat())}, f
         )
     return calcs
 
 
 if __name__ == "__main__":
-    print(fetch())
+    print(cache())
