700006e0b7ad0d7098197e48fd94e9b3e5f50ada
HeyItsJono <freymotifs@gmail.com>
2024-09-29 05:31:03 +1000

    Add filtering logic.

--- plugin/flowmd.py
+++ plugin/flowmd.py
@@ -10,20 +10,44 @@ calcs = plugin.calcs.fetch()
 class flowMD(FlowLauncher):
     def query(self, query):
         results = []
-
-        for calc in calcs:
+        if query == "":
+            for calc in calcs:
+                results.append(
+                    {
+                        "Title": calc["title"],
+                        "SubTitle": calc["description"],
+                        "IcoPath": "assets/mdcalc.png",
+                        "JSONRPCAction": {
+                            "method": "open_url",
+                            "parameters": [calc["url"]],
+                        },
+                    }
+                )
+        elif len(query) > 0:
+            for calc in calcs:
+                if (
+                    query in calc["title"].lower()
+                    or query in calc["description"].lower()
+                ):
+                    results.append(
+                        {
+                            "Title": calc["title"],
+                            "SubTitle": calc["description"],
+                            "IcoPath": "assets/mdcalc.png",
+                            "JSONRPCAction": {
+                                "method": "open_url",
+                                "parameters": [calc["url"]],
+                            },
+                        }
+                    )
+        if not results:
             results.append(
                 {
-                    "Title": calc["title"],
-                    "SubTitle": calc["description"],
-                    "IcoPath": "assets/mdcalc.svg",
-                    "JSONRPCAction": {
-                        "method": "open_url",
-                        "parameters": [calc["url"]],
-                    },
+                    "Title": "No results found",
+                    "SubTitle": "Try searching for something else.",
+                    "IcoPath": "assets/mdcalc.png",
                 }
             )
-
         return results
 
     def context_menu(self, data):
@@ -31,7 +55,7 @@ class flowMD(FlowLauncher):
             {
                 "Title": "Add to Favourites",
                 "SubTitle": "Favourite calculators are shown above the others.",
-                "IcoPath": "assets/mdcalc.svg",
+                "IcoPath": "assets/mdcalc.png",
                 "JSONRPCAction": {
                     "method": "open_url",
                     "parameters": ["https://www.mdcalc.com/"],
